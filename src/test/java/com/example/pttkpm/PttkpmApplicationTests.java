02eaee2a35e32c277a5787302ee56209568d26db
Nguyendat0819 <23010282@st.phenikaa-uni.edu.vn>
2025-09-17 22:50:50 +0700

    'project'

---
+++ src/test/java/com/example/pttkpm/PttkpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pttkpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PttkpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
