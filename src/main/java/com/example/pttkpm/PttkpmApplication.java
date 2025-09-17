d18e41a1f7833d5e17153eeb3ec68359dbb05e5b
Nguyendat0819 <23010282@st.phenikaa-uni.edu.vn>
2025-09-17 22:50:50 +0700

    'project'

---
+++ src/main/java/com/example/pttkpm/PttkpmApplication.java
@@ -0,0 +1,13 @@
+package com.example.pttkpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PttkpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PttkpmApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/pttkpm/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.example.pttkpm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class AdminController {
+    @GetMapping("/admin/AdminPage")
+    public String AdminPageView() {
+        return "admin/AdminPage";
+    }
+    
+}
